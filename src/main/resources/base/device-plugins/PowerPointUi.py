8487aacf97d886d0f63b508f2ac70f3536e6d0e6
MacLim <xmachd@gmail.com>
2020-11-24 18:32:31 +1100

    add obs control and update powerPointControl

--- src/main/resources/base/device-plugins/PowerPointUi.py
+++ src/main/resources/base/device-plugins/PowerPointUi.py
@@ -3,15 +3,21 @@ import sys, os
 import win32com, win32com.client
 from pathlib import Path
 from PyQt5 import uic, QtWidgets
+import Ui_PowerPoint
 
 #Power Point Controller
-class PowerPoint():
+class PowerPointUi(QtWidgets.QMainWindow):
     def __init__(self, a_id=1):
         """Create new powerpoint control interface"""
+        super(PowerPointUi, self).__init__()
         self.id = a_id
         raw_path = Path(os.getcwd()).parent
         self.pptdir = raw_path.__str__() + "/media/"
         print("Loading: "+str(self.pptdir))
+
+        #establish gui
+        self.uippt = Ui_PowerPoint.Ui_MainWindow()
+        self.uippt.setupUi(self)
     
     def connect_com32(self):
         """Connect to Power Point Instance"""
@@ -50,3 +56,24 @@ class PowerPoint():
     def ppt_quit(self):
         """Quit Powerpoint (NOT FUNCTIONAL)"""
         self.a.Quit()
+
+    def create_slots(self):
+        #slots
+        self.uippt.loadfile.clicked.connect(lambda: self.ppt_open(str(self.uippt.filebox.text())))
+        self.uippt.prevslide.clicked.connect(lambda: self.ppt_ps())
+        self.uippt.nextslide.clicked.connect(lambda: self.ppt_ns())
+        self.uippt.gotoslide.clicked.connect(lambda: self.ppt_gs(str(self.uippt.slidenum.text())))
+        self.uippt.startslideshow.clicked.connect(lambda: self.ppt_ss())
+
+    def closeEvent(self, event):
+        print("Shutting down Power Point Handler")
+        self.ppt_close()
+        self.ppt_quit()
+
+    def createGUI(self, instance):
+        """Create Control GUI"""
+        app = QtWidgets.QApplication(sys.argv)
+        window = instance
+        window.create_slots()
+        window.show()
+        app.exec_()

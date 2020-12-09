0c25d2c437e393192c641151ffacdb0b1c85021c
MacLim <xmachd@gmail.com>
2020-12-09 16:55:10 +1100

    Update to Mcntrl

--- src/main/resources/base/TCPSocketHost.py
+++ src/main/resources/base/TCPSocketHost.py
@@ -7,7 +7,7 @@ listen_address = '0.0.0.0'
 port = 7000
 """Setup/import device_plugins"""
 obs_stream = obs.OBS(ip_port="4444", already_open=True)
-obs_display = obs.OBS(ip_port="4445", already_open=True)
+obs_display_one = obs.OBS(ip_port="4445", already_open=True)
 gefen = GefenMatrixControl.EXTHDSL444(ip_address="192.168.0.1", ip_port=23)
 """Map message to action"""
 class Echo(Protocol):
@@ -21,15 +21,56 @@ class Echo(Protocol):
             obs_stream.connect()
             print("Connecting to OBS Stream")
         if target == "obs.display.connect":
-            obs_display.connect()
+            obs_display_one.connect()
             print("Connecting to OBS Display")
         if target == "obs.stream.setScene":
             obs_stream.setScene(cmd[1])
-            print("Setting Scene on OBS Stream")
+            print("Setting Scene " +str(cmd[1])+ "on OBS Stream")
         if target == "obs.display.setScene":
-            obs_display.setScene(cmd[1])
-            print("Setting Scene on OBS Display")
-
+            obs_display_one.setScene(cmd[1])
+            print("Setting Scene:" +str(cmd[1])+ "on OBS Display")
+        if target == "obs.stream.transition":
+            obs_stream.transition()
+            print("Transitioning on OBS Stream")
+        if target == "obs.display.transition":
+            obs_display_one.transition()
+            print("Transitioning on OBS Display")
+        if target == "obs.stream.toggleStudioMode":
+            obs_stream.toggleStudioMode()
+            print("Toggling studio mode on OBS Stream")   
+        if target == "obs.display.toggleStudioMode":
+            obs_display_one.toggleStudioMode()
+            print("Toggling studio mode on OBS Display")   
+        if target == "obs.stream.startRecording":
+            obs_stream.startRecording()
+            print("Starting recording on OBS Stream")
+        if target == "obs.display.startRecording":
+            obs_display_one.startRecording()
+            print("Starting recording on OBS Display")
+        if target == "obs.stream.stopRecording":
+            obs_stream.stopRecording()
+            print("Stopped recording on OBS Stream")
+        if target == "obs.display.stopRecording":
+            obs_display_one.stopRecording()
+            print("Stopped recording on OBS Display")
+        #gefen control
+        if target == "gefen.connectTelnet":
+            print("Using user: "+str(cmd[1])+" | password: "+str(cmd[2]))
+            gefen.connect_telnet(login_user=str([cmd[1]]), login_pass=str(cmd[2]))
+            print("Connecting to Gefen Matrix via TELNET")
+        if target == "gefen.set1":
+            gefen.map(1, cmd[1])
+            print("Setting Gefen screen 1 to "+str(cmd[1]))
+        if target == "gefen.set2":
+            gefen.map(2, cmd[1])
+            print("Setting Gefen screen 2 to "+str(cmd[1]))
+        if target == "gefen.set3":
+            gefen.map(3, cmd[1])
+            print("Setting Gefen screen 3 to "+str(cmd[1]))
+        if target == "gefen.set4":
+            gefen.map(4, cmd[1])
+            print("Setting Gefen screen 4 to "+str(cmd[1]))
+        
 def main():
     f = Factory()
     f.protocol = Echo
